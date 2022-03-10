0723bf4a011a335ab40a44aded04707a9f7c6e67
makbulekayapinar <makbule-kayapnar@hotmail.com>
2022-03-10 22:48:05 +0300

    Create VucutKitleIndex.java
    Kullanıcıdan boy ve kilo alarak vücut kitle indexi hesaplayan program

---
+++ VucutKitleIndex.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class VucutKitleIndex {
+    public static void main(String[] args) {
+        double boy,kilo,vucutIndex;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+
+        boy = input.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz : ");
+
+        kilo = input.nextDouble();
+
+        vucutIndex =  kilo / boy * boy;
+        System.out.print("Vücut Kitle İndeksiniz :" + vucutIndex);
+
+    }
+
+}
